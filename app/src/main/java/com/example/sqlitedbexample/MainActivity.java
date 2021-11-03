51ecc2da6a9f304d574963146e1fb000028142d5
Swathi P G <eng.swathiprasad@gmail.com>
2021-11-03 11:49:01 +0000

    Functions combined

--- app/src/main/java/com/example/sqlitedbexample/MainActivity.java
+++ app/src/main/java/com/example/sqlitedbexample/MainActivity.java
@@ -22,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnCreate= findViewById(R.id.btnCreate);
-        btnInsert= findViewById(R.id.btnInsert);
+        initialiseViews();
 
         db_path = MainActivity.this.getDatabasePath("Country").getPath();
 
@@ -56,5 +55,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void initialiseViews() {
+
+        btnCreate= findViewById(R.id.btnCreate);
+        btnInsert= findViewById(R.id.btnInsert);
     }
 }
